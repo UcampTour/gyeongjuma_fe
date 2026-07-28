ca8f4d822de0a584da7df1a50237528e878a5de3
주현석 <wngustjr1306@naver.com>
2026-07-28 11:28:40 +0900

    http 우회 추가

--- src/api/authService.ts
+++ src/api/authService.ts
@@ -6,21 +6,21 @@ import { authClient } from "./authClient";
 // 여기서 AuthStore 안쓰는 방향으로...
 // 1. 소셜 로그인
 export const login = async (request: SignupRequest): Promise<SingupResponse> => {
-  const response = await apiClient.post<ApiResponse<SingupResponse>>("/api/members/login", request);
+  const response = await apiClient.post<ApiResponse<SingupResponse>>("/members/login", request);
 
   return response.data.data;
 }
 
 // 2. 닉네임 중복 확인 
 export const checkNickname = async (request: CheckNicknameRequest): Promise<CheckNicknameResponse> => {
-  const response = await apiClient.get("/api/members/check-nickname",{ params: { nickname: request.nickname} });
+  const response = await apiClient.get("/members/check-nickname",{ params: { nickname: request.nickname} });
 
   return response.data.data;
 }
 
 // 3. 추가 정보 등록
 export const registerExtraInfo = async (request: ExtraInfoRequest): Promise<ExtraInfoResponse> => {
-  const response = await apiClient.patch("/api/members/extra-info", request);
+  const response = await apiClient.patch("/members/extra-info", request);
 
   return response.data.data;
 }
@@ -29,7 +29,7 @@ export const registerExtraInfo = async (request: ExtraInfoRequest): Promise<Extr
 export const reissue = async (): Promise<RefreshTokenResponse> => {
   const refreshToken = useAuthStore.getState().refreshToken;
 
-  const response = await authClient.post("/api/members/reissue", { refreshToken });
+  const response = await authClient.post("/members/reissue", { refreshToken });
 
   return response.data.data;
 }
@@ -37,11 +37,11 @@ export const reissue = async (): Promise<RefreshTokenResponse> => {
 // 5. 로그아웃
 export const logout = async (): Promise<void> => {
 
-  await apiClient.post("/api/members/logout");
+  await apiClient.post("/members/logout");
 };
 
 //6. 회원 탈퇴
 export const withdraw = async (): Promise<void> => {
   
-  await apiClient.delete("/api/members");
+  await apiClient.delete("/members");
 }
